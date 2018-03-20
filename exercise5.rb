1d342dc9c914fd6f91553973d41513cd75f247bf
Karen <kjgamis@live.ca>
2018-03-20 15:58:25 -0400

    finish exercise 5

--- exercise5.rb
+++ exercise5.rb
@@ -1,3 +1,9 @@
 def greet_backwards(name)
   return 'Hello, ' + name.reverse + name.reverse + '! Welcome home.'
 end
+
+p greet_backwards('Amanda')
+p greet_backwards('Bob')
+p greet_backwards('Shirly')
+p greet_backwards('Sue')
+p greet_backwards('Andy')
